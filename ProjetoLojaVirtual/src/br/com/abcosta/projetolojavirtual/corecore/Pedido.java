34401e6ea9abc8f571a9691f3b0b5f31abf302b7
ab-costa <costa.alessandroborges@gmail.com>
2022-02-28 11:38:30 -0300

    Projeto loja virtual

---
+++ ProjetoLojaVirtual/src/br/com/abcosta/projetolojavirtual/corecore/Pedido.java
@@ -0,0 +1,44 @@
+package br.com.abcosta.projetolojavirtual.corecore;
+
+public class Pedido {
+	private String data;
+	private Cliente cliente;
+	private Produto produto;
+	
+	public Pedido(String data, Cliente cliente, Produto produto) {
+		super();
+		this.data = data;
+		this.cliente = cliente;
+		this.produto = produto;
+	}
+	
+	public String showInfos() {
+		return "Pedido" + this.getData() + "\n" +
+					"\t" + "Produto: " + this.getProduto().showInfos() + "\n" +
+						"\t" + "Cliente: " + this.getCliente().showInfos();
+	}
+	
+	public String getData() {
+		return data;
+	}
+	
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	public Produto getProduto() {
+		return produto;
+	}
+	
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+}
---
+++ ProjetoLojaVirtual/src/br/com/abcosta/projetolojavirtual/ui/Main.java
@@ -0,0 +1,24 @@
+package br.com.abcosta.projetolojavirtual.ui;
+
+import br.com.abcosta.projetolojavirtual.corecore.Cliente;
+import br.com.abcosta.projetolojavirtual.corecore.Pedido;
+import br.com.abcosta.projetolojavirtual.corecore.Produto;
+
+public class Main {
+	public static void main(String[] args) {
+		Produto mostruario[] = new Produto[3];
+		
+		mostruario[0] = new Produto(1001, "Produto um", 15.00);
+		mostruario[1] = new Produto(1002, "Produto dois", 25.00);
+		mostruario[2] = new Produto(1003, "Produto três", 75.00);
+		
+		Cliente clientes[] = new Cliente[2];
+		
+		clientes[0] = new Cliente(2001, "Cliente Um", "clienteUm@email.com");
+		clientes[1] = new Cliente(2002, "Cliente Dois", "clienteDois@email.com");
+		
+		Pedido pedido = new Pedido("28/02/2022", clientes[0], mostruario[2]);
+		
+		System.out.println(pedido.showInfos());
+	}
+}
